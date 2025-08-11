a90d8e35f599771861b4d3c3c93eef6c19bd1efd
atharva0311 <73391900+atharva0311@users.noreply.github.com>
2025-08-11 11:57:28 +0530

    Update settings.py

--- Project/settings.py
+++ Project/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'Project/static'),
  # path where your images/css/js are stored
 ]
 
